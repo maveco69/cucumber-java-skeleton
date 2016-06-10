20e1db782152dbac0deb9711aa9a6344a4bb4f53
André de Lima e Silva <andre.lima@softplan.com.br>
2016-06-10 18:18:38 -0300

    Seed

--- src/main/java/skeleton/Calculadora.java
+++ src/main/java/skeleton/Calculadora.java
@@ -1,33 +1,5 @@
 package skeleton;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Calculadora {
 
-    private List<BigDecimal> numeros = new LinkedList<>();
-    private BigDecimal result;
-
-    public void adicionaNumero(BigDecimal n) {
-        numeros.add(n);
-    }
-
-    public void soma() {
-        result = numeros.stream().reduce(BigDecimal::add).get();
-    }
-
-    public BigDecimal getResult() {
-        return result;
-    }
-
-    public void limpar() {
-        numeros.clear();
-        result = BigDecimal.ZERO;
-    }
-
-    public void divide() {
-        result = numeros.stream().reduce(BigDecimal::divide).get();
-    }
 }
--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,40 +1,5 @@
 package skeleton;
 
-import cucumber.api.java.Before;
-import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Entao;
-import cucumber.api.java.pt.Quando;
-import org.assertj.core.api.Assertions;
-
-import java.math.BigDecimal;
-
 public class Stepdefs {
-
-    private Calculadora calculadora = new Calculadora();
-
-    @Before
-    public void setUp() {
-        calculadora.limpar();
-    }
-
-    @Dado("^que informo o número \"([^\"]*)\"$")
-    public void que_informo_o_numero(BigDecimal number) throws Throwable {
-        calculadora.adicionaNumero(number);
-    }
-
-    @Quando("^realizo a soma dos números$")
-    public void realizo_a_soma_dos_n_meros() throws Throwable {
-        calculadora.soma();
-    }
-
-    @Entao("^o resultado deve ser \"([^\"]*)\"$")
-    public void o_resultado_deve_ser(BigDecimal number) throws Throwable {
-        Assertions.assertThat(calculadora.getResult()).isEqualByComparingTo(number);
-    }
-
-    @Quando("^realizo a divisao dos números$")
-    public void realizo_a_divisao_dos_n_meros() throws Throwable {
-        calculadora.divide();
-    }
 
 }
